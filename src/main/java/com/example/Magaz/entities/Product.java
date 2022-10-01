ec8f7a32ba32f49acf1f1f0af2d0514d5ec64b51
Printsevskaya <helloman005@gmail.com>
2022-10-01 17:06:19 +0300

    Changes

---
+++ src/main/java/com/example/Magaz/entities/Product.java
@@ -0,0 +1,23 @@
+package com.example.Magaz.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+public class Product {
+//    @Entity
+//    @Getter
+//    @Setter
+//    @NoArgsConstructor
+//    public class Magazine {
+//        @Id
+//        private UUID id = UUID.randomUUID();
+//        @NotNull
+//        private String nameMagazine;
+//        private String addressMagazine;
+}
