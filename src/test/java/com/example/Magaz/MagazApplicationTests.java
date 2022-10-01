7de49be0d077d8eb4c85da6c971c05510f899412
Printsevskaya <helloman005@gmail.com>
2022-10-01 17:25:43 +0300

    Changes

---
+++ src/test/java/com/example/Magaz/MagazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Magaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
